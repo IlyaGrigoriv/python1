fc017295a2425781d09daaaddef972b36ce61ad4
Ilya Grigorev <89213248144@mail.ru>
2023-02-16 20:03:57 +0300

    дз5

--- homejob04/main22.py
+++ homejob04/main22.py
@@ -3,33 +3,28 @@
 # встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n - кол-во элементов первого множества. m - кол-во
 # элементов второго множества. Затем пользователь вводит сами элементы множеств.
-# from random import randint
-# a = int(input('Ввидите кол-во элементов первого множества: '))
-# b = int(input('Ввидите кол-во элементов второго множества: '))
+
 from random import randint
 
-n = 4#int(input("Ввидите число: "))
-m = 5#int(input("Ввидите число: "))
-list1 = {1}
-list2 = {2}
-# list1 = n,m
-# print(list1)
-
-
+n = int(input("Ввидите число: "))
+m = int(input("Ввидите число: "))
+list1 = []#[ i+1 for i in range(1,20) if i %2 ==0]
+list2 = []#[ i+1 for i in range(1,20) if i %2 ==0]
 
 def NewMany(list,n):
 
     for i in range(n):
-        list.add(randint(1, 99))
+        list.add(randint(1, 5))
+    #print(list)
     return list
 
 
 list1 = NewMany(list1,n)
 list2 = NewMany(list2,m)
-print(list1)
-print(list2)
-# list3 = {}
+print(f'первый лист {list1}')
+print(f'второй лист {list2}')
+list3 = {0}
 list3 = list1.intersection(list2)
-print(list3)
+print(f'итог {list3}')
 
 #работает
--- les03/main01.py
+++ les03/main01.py
@@ -1,5 +1,15 @@
-n = 5
-s = 0
-for i in range(1, n+1):
-    s += i
-    print(s)
+# n = 5
+# s = 0
+# for i in range(1, n+1):
+#     s += i
+#     print(s)
+
+n = int(123)
+print(n)
+n = str(n)
+print(n+n)
+n = int(n)
+print(n+n)
+n = -12
+n = abs(n)
+print(n)
