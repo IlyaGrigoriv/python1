5fd86e2072959b3b41cf98af86bced73855efd5e
Ilya Grigorev <89213248144@mail.ru>
2023-02-20 09:30:09 +0300

    дз6

--- homeJob6/main30.py
+++ homeJob6/main30.py
@@ -5,10 +5,12 @@
 # Каждое число вводится с новой строки.
 
 num,step,size = int(input()),int(input()),int(input())
-list_1 = [size]
-
-for i in range(num,size,step):
-    list_1.append(i)
+list_1 = []
+count = num
+while size>0:
+    list_1.append(count)
+    size-=1
+    count+=step
 print(list_1) 
 # for i in range(num,long+num,step ):
 #     print(i)
